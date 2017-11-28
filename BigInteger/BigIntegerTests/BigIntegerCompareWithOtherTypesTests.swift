dabbdea717c42ad198b5b9f23a512c3aeb401bcd
Vladimir Abramichev <descorp134@mail.ru>
2017-11-28 20:00:09 +0100

    Tests for comparing with primitive types

---
+++ BigInteger/BigIntegerTests/BigIntegerCompareWithOtherTypesTests.swift
@@ -0,0 +1,62 @@
+//
+//  BigIntegerCompareWithOtherTypesTests.swift
+//  BigIntegerTests
+//
+//  Created by Vladimir Abramichev on 28/11/2017.
+//  Copyright © 2017 Vladimir Abramichev. All rights reserved.
+//
+
+import XCTest
+
+class BigIntegerCompareWithOtherTypesTests: XCTestCase {
+    
+    func testEquility() {
+        let b = BigInteger(value: -42)
+        let a = 42
+        
+        XCTAssertTrue(a != b)
+        XCTAssertTrue(b != a)
+        XCTAssertTrue(a == -b)
+        XCTAssertTrue(b == -a)
+        XCTAssertTrue(a == a)
+        XCTAssertTrue(b == b)
+    }
+    
+    func testCompareWithZero() {
+        let a = BigInteger()
+        let b = -42
+        let c = BigInteger(value: 42)
+        
+        XCTAssertTrue(a > b, " \(a) > \(b)")
+        XCTAssertTrue(b < a)
+        XCTAssertTrue(a >= b)
+        XCTAssertTrue(b <= a)
+        XCTAssertTrue(c > a)
+        XCTAssertTrue(a < c)
+        XCTAssertTrue(c >= a)
+        XCTAssertTrue(a <= c)
+    }
+    
+    func testCompare() {
+        let a = BigInteger(value: 42)
+        let b = -42
+        
+        XCTAssertTrue(a > b)
+        XCTAssertTrue(b < a)
+        XCTAssertTrue(a >= b)
+        XCTAssertTrue(b <= a)
+    }
+    
+    func testNegation() {
+        let a = BigInteger(value: -5)
+        let b = 5
+        
+        XCTAssertFalse(a.sign)
+        XCTAssertTrue((-a).sign)
+        
+        XCTAssertFalse(a == b, "a != b")
+        XCTAssertTrue(a == -b, "a = -b")
+        XCTAssertTrue(-a == b, "-a = b")
+        XCTAssertFalse(-a == -b, "-a != -b")
+    }
+}
